3e2b5d498007d739f4cb1525c80950b6eed9a959
krzysztofo <comodore9636@gmail.com>
2025-10-27 16:42:17 +0100

    sasd

--- scribe-service/app/utils.py
+++ scribe-service/app/utils.py
@@ -112,7 +112,10 @@ def scribe(
     src_code, std_code = to_mbart50(lang_classifier(text[:300])[0]['label']), to_mbart50("en_XX")
     text_en = translate_text(text, src_lang=src_code, tgt_lang=std_code) if src_code != std_code else text
     chunk_size = 3072
-    chunks = [f"<text>{text_en[i:i+chunk_size]}</text>" for i in range(0, len(text_en), chunk_size)]
+    chunks = [
+        f"<text>{text_en[i:min(i+chunk_size, len(text_en) - 1)]}</text>" 
+        for i in range(0, len(text_en), chunk_size)
+    ]
     summaries = summarizer(chunks)
     summaries = ["##" + summary['summary_text'].split("##", 1)[-1] for summary in summaries]
     summary = "# Summary\n\n" + "\n\n".join(summaries)
